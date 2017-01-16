a029e6e73b1adf0a40be45c4a7cba03a80579a64
stttt2003pk <stttt2003pk@gmail.com>
2017-01-16 11:43:11 +0800

    finishe writing some get machine information and ipvsadm -ln

--- run.py
+++ run.py
@@ -75,7 +75,267 @@ class Node(object):
 
         return json.dumps(cpu, sort_keys=False, indent=4, separators=(',', ': '))
 
+    '''
+    uri /node/GetMemInfo/
+    '''
+    @cherrypy.expose
+    def GetMemInfo(self):
+        mem = {}
+        f = open("/proc/meminfo")
+        lines = f.readlines()
+        f.close()
+        
+        for line in lines:
+            if len(line) < 2:
+                continue
+            name = line.split(':')[0]
+            var = line.split(':')[1].split()[0]
+            mem[name] = long(var)*1024.0
+        mem['MemUsed'] = mem['MemTotal'] - mem['MemFree'] - mem['Buffers'] - mem['Cached']
+        return json.dumps(mem, sort_keys=False, indent=4, separators=(',', ': '))
+
+    '''
+    uri /node/GetLoadAvg/
+    '''
+    @cherrypy.expose
+    def GetLoadAvg(self):
+        loadavg = {}
+        f = open("/proc/loadavg") 
+        load = f.read().split()
+        f.close()
+        
+        loadavg['lavg_1']=load[0]
+        loadavg['lavg_5']=load[1]
+        loadavg['lavg_15']=load[2]
+        loadavg['nr']=load[3]
+        loadavg['last_pid']=load[4]
+
+        return json.dumps(loadavg, sort_keys=False, indent=4, separators=(',', ': '))
+
+    '''
+    uri /node/GetIfInfo/centos7_interface
+    '''
+    @cherrypy.expose
+    def GetIfInfo(self, interface):
+        f = open("/proc/net/dev")
+        lines = f.readlines()
+        f.close()
+
+        intf = {}
+
+        for line in lines:
+            con = line.split()
+            offset = con[0].split(':')
+            if str(offset[0]) == interface:
+                intf['interface'] = str(offset[0])
+                intf['ReceiveBytes'] = str(offset[1])
+                intf['ReceivePackets'] = str(con[1])
+                intf['ReceiveErrs'] = str(con[2])
+                intf['ReceiveDrop'] = str(con[3])
+                intf['ReceiveFifo'] = str(con[4])
+                intf['ReceiveFrames'] = str(con[5])
+                intf['ReceiveCompressed'] = str(con[6])
+                intf['ReceiveMulticast'] = str(con[7])
+                intf['TransmitBytes'] = str(con[8])
+                intf['TransmitPackets'] = str(con[9])
+                intf['TransmitErrs'] = str(con[10])
+                intf['TransmitDrop'] = str(con[11])
+                intf['TransmitFifo'] = str(con[12])
+                intf['TransmitFrames'] = str(con[13])
+                intf['TransmitCompressed'] = str(con[14])
+                intf['TransmitMulticast'] = str(con[15])
+
+                return json.dumps(intf, sort_keys=False)
+
+    '''
+    get the traffic from net dev
+    '''
+    @cherrypy.expose
+    def GetIfTraffic(self):           
+        ifs = []
+        nettraffic = {}
+        f = open("/proc/net/dev")
+        lines = f.readlines()
+        f.close()
+        
+        for line in lines[2:]:
+            con = line.split()
+            ifname = con[0].split(':') 
+            if(ifname[0].strip() != 'lo'):
+                ifs.append(ifname[0].strip())
+            else:
+                continue
+        for interface in ifs:
+            nettraffic[interface] = self.GetIfInfo(interface)
+
+        return json.dumps(nettraffic)
+        
+    '''
+    get the infomation of df -h
+    uri /node/GetHddInfo/
+    '''
+    @cherrypy.expose
+    def GetHddInfo(self):
+        hdds = []
+        mount = {}
+        file_system = []
+        type = []
+        size = []
+        used = []
+        avail = []
+        used_percent = []
+        mounted_on = []
+        hdds = os.popen('df -lhT  | grep -v tmpfs | grep -v boot | grep -v usr | grep -v tmp | sed \'1d;/ /!N;s/\\n//;s/[ ]*[ ]/\\t/g;\'').readlines()
+        for line in hdds:
+            dict = {}
+            file_system = line.replace('\\n','').replace('\\t',' ').split()[0]
+            dict['type'] = line.replace('\\n','').replace('\\t',' ').split()[1]
+            dict['size'] = line.replace('\\n','').replace('\\t',' ').split()[2]
+            dict['used'] = line.replace('\\n','').replace('\\t',' ').split()[3]
+            dict['avail'] = line.replace('\\n','').replace('\\t',' ').split()[4]
+            dict['used_percent'] = line.replace('\\n','').replace('\\t',' ').split()[5]
+            dict['mounted_on'] = line.replace('\\n','').replace('\\t',' ').split()[6]
+            dict['file_system'] = file_system
+            mount[file_system] = dict
+
+        return json.dumps(mount) 
+
+    '''
+    get cpu usage
+    uri /node/GetCpuDetail/
+    '''
+    @cherrypy.expose
+    def GetCpuDetail(self):
+        dist_json = self.dist()
+        dist = json.loads(dist_json)
+        if(dist['os.system'] in ['CentOS', 'centos', 'redhat', 'RedHat']):
+            if(int(dist['os.version'].split('.')[0])  < 6):  #For CentOS only 
+                cmd = 'mpstat 1 1 | sed \'1d;2d;3d;4d\' | awk \'{print "{\\\"user\\\":\\\"\"$3\"\\\",\\\"nice\\\":\\\"\"$4\"\\\",\\\"sys\\\":\\\"\"$5\"\\\",\\\"iowait\\\":\\\"\"$6\"\\\",\\\"irq\\\":\\\"\"$7\"\\\",\\\"soft\\\":\\\"\"$8\"\\\",\\\"steal\\\":\\\"\"$9\"\\\",\\\"idle\\\":\\\"\"$10\"\\\"}"}\''
+            else:
+                cmd = 'mpstat 1 1 | sed \'1d;2d;3d;4d\' | awk \'{print "{\\\"user\\\":\\\"\"$3\"\\\",\\\"nice\\\":\\\"\"$4\"\\\",\\\"sys\\\":\\\"\"$5\"\\\",\\\"iowait\\\":\\\"\"$6\"\\\",\\\"irq\\\":\\\"\"$7\"\\\",\\\"soft\\\":\\\"\"$8\"\\\",\\\"steal\\\":\\\"\"$9\"\\\",\\\"idle\\\":\\\"\"$12\"\\\"}"}\''
+        else:
+            cmd = 'mpstat 1 1 | sed \'1d;2d;3d;4d\' | awk \'{print "{\\\"user\\\":\\\"\"$3\"\\\",\\\"nice\\\":\\\"\"$4\"\\\",\\\"sys\\\":\\\"\"$5\"\\\",\\\"iowait\\\":\\\"\"$6\"\\\",\\\"irq\\\":\\\"\"$7\"\\\",\\\"soft\\\":\\\"\"$8\"\\\",\\\"steal\\\":\\\"\"$9\"\\\",\\\"idle\\\":\\\"\"$11\"\\\"}"}\''
+        cpu = os.popen(cmd).readline().strip()
+        return cpu
+
+
+    '''
+    get /proc/net/ip_vs_stats
+    '''
+    @cherrypy.expose
+    def GetLvsStatsSumm(self):
+        stats = {}
+        conns = []
+        in_pks = []
+        out_pks = []
+        in_bytes = []
+        out_bytes = []
+        
+        f = open("/proc/net/ip_vs_stats")
+        lines = f.readlines()
+        f.close()
+
+        conns.append(int(lines[2].split()[0], 16)) 
+        in_pks.append(int(lines[2].split()[1], 16))
+        out_pks.append(int(lines[2].split()[2], 16))
+        in_bytes.append(int(lines[2].split()[3], 16))
+        out_bytes.append(int(lines[2].split()[4], 16))
+
+        stats = {"conns":conns,"in_pks":in_pks,"out_pks":out_pks,"in_bytes":in_bytes,"out_bytes":out_bytes}
+        return json.dumps(stats, sort_keys=False, indent=4, separators=(',', ': '))
+
+    '''
+    get lvs rs stats
+    uri /node/GetLvsConn/
+    '''
+    @cherrypy.expose
+    def GetLvsConn(self):
+        Conn = []
+        node_list = []
+        dict = {}
+        num = 0
+        cmd = "ipvsadm -ln"
+        lines = os.popen(cmd).readlines()
+
+        for line in lines[3:]:
+            num += 1
+            con = line.split()
+            if con[0] == "TCP" or con[0] == "UDP":
+                if num == 1:
+                    pass
+                else:
+                    Conn.append(dict)
+                dict = {}
+                dict['lb_algo'] =  str(con[2])
+                dict['vip'] = str(con[1])
+                dict['node'] = []
+                continue
+            node_dict = {"rs":con[1],"lb_kind":con[2],"weight":con[3],"activeconn":con[4],"inactconn":con[5]}
+            dict['node'].append(node_dict)
+            if num == len(lines[3:]):
+                Conn.append(dict)
+        
+        return json.dumps(Conn, sort_keys=False, indent=4, separators=(',', ': '))
+
+    '''
+    get GetLvsStatus
+    uri /node/GetLvsStatus
+    '''
+    @cherrypy.expose
+    def GetLvsStatus(self):
+        Conn = []
+        node_list = []
+        dict = {}
+        num = 0
+        cmd = "ipvsadm -ln"
+        lines = os.popen(cmd).readlines()
+        for line in lines[3:]:
+            num += 1
+            con = line.split()
+            if con[0] == "TCP" or con[0] == "UDP":
+                if num == 1:
+                    pass
+                else:
+                    Conn.append(dict)
+                dict = {}
+                dict['lb_algo'] = str(con[2])
+                dict['vip'] = str(con[1])
+                dict['node'] = []
+                continue
+            node_dict = {"rs":con[1],"lb_kind":con[2],"weight":con[3]}
+            dict['node'].append(node_dict)
+            if num == len(lines[3:]):
+                Conn.append(dict)
+        
+        return json.dumps(Conn, sort_keys=False, indent=4, separators=(',', ': '))
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #
+
 if "__main__" == __name__:
     settings = {
                 'global': {
