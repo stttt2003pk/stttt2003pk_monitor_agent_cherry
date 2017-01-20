4e5e34dbcb50c7c2cf666d2f1daea9743b3e0f66
stttt2003pk <stttt2003pk@gmail.com>
2017-01-20 11:47:57 +0800

    bug in log matching regular expression

--- lvs_logAnalyze_stttt2003pk.py
+++ lvs_logAnalyze_stttt2003pk.py
@@ -21,16 +21,16 @@ import requests
 import Queue
 import threading
 
-job_queue = Queue.Queue(0)
 
 class keepalived_log_analyze():
     def __init__(self, filename, logfile):
+        self.filename = filename
         self.file = open(self.filename,'r')
         self.file_stat = os.stat(self.filename)
         self.file_size = self.file_stat[6]
         self.file.seek(self.file_size)
         try:
-            loggin.basicConfig)(
+            logging.basicConfig(
                 level=logging.NOTSET,
                 format='%(asctime)s %(levelname)-8s %(message)s',
                 datefmt='%Y-%m-%d %H:%M:%S',
@@ -47,6 +47,7 @@ class keepalived_log_analyze():
     def service_is_disable(self, log):
         compile_log = re.compile(r'^(\w*)\s*(\d+)\s*(\d+:\d+:\d+)\s*(\S*)\s*(\w*):\s*Executing\s*\[ip\s*addr\s*del.*\]\s*\for\s*VS\s*\[(\S*)\]:\d+$')
         match = compile_log.match(log)
+        print match
 
         if match:
             log_group = match.groups()
@@ -59,7 +60,7 @@ class keepalived_log_analyze():
                 "job_type":"run_service_is_disable_action",
                 "lb_id":lb_id,
                 "vip_instance":vip_instance,
-                ,"alert_time":alert_time,
+                "alert_time":alert_time,
             }
 
             job_queue.put(job)
@@ -128,8 +129,33 @@ class keepalived_log_analyze():
             }
 
 
+    #handler
+    def handler(self, line):
+        self.rs_is_disable(line)
+        self.rs_is_enable(line)
+        self.service_is_disable(line)
+        self.service_is_up(line)
 
+    #run the worker in
+    def run(self):
+        while True:
+            time.sleep(1)
+            lines = self.file.readlines()
+            if not lines:
+                continue
+            for line in lines:
+                self.handler(line)
 
+job_queue = Queue.Queue(0)
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+config = yaml.load(open(os.path.join(cur_dir,'config.yaml')))
+
+if __name__ == '__main__':
+    filename = config['keepalived_log_file']
+    logfile = config['logfile']
+    
+    work = keepalived_log_analyze(filename,logfile)
+    work.run()
 
 
 
